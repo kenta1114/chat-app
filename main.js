d970b894b939be453fdeeb5f7cd3e80b971fd761
kenta1114 <61451524+kenta1114@users.noreply.github.com>
2025-09-19 04:39:48 +0000

    fix ui chatapp

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+// GPUアクセラレーション関連の設定
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('no-sandbox');
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+
 const path = require('path');
 
 function createWindow(){
   //メインウインドウを作成
-  const mainWindow = new BrowserWindow({
-    width:900,
-    height:700,
-    webPreferences:{
-      nodeIntegration:true,
-      contextIsolation:false
-    },
-
-    //アイコンファイル
-    //icon:path.join(__dirname, 'icon.png'),
-    tiitleBarStyle:'default',
-    resizable:true,
-    minWidth:600,
-    minWidth:600,
-    minHeight:500,
-    show:false
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
   });
 
-  //HTMLファイルを読み込み
-  mainWindow.loadFile('index.html');
-
-  //ウインドウの準備ができたら表示
-  mainWindow.once('ready-to-show',()=>{
-    mainWindow.show();
-  });
-
-  //開発時のみDevToolsを開く(必要に応じてコメントアウト)
-  
+  // 開発モードとして扱い、Viteの開発サーバーに接続
+  win.loadURL("http://localhost:5173");
 }
+
+const { ipcMain } = require('electron');
+
+ipcMain.on("message", (event,data)=>{
+  console.log("Reactから:"+data);
+
+  event.sender.send("reply", "受け取りました！");
+});
 
 //アプリが準備完了したらウインドウを作成
 //古いElectronバージョン対応
@@ -43,7 +43,7 @@ if(app.whenReady){
   app.on('ready',createWindow);
 }
 
-//すべてのウインドwが閉じられたらアプリを終了(macOS以外)
+//すべてのウインドウが閉じられたらアプリを終了(macOS以外)
 app.on('window-all-closed', ()=>{
   if(process.platform !== 'darwin'){
     app.quit();
@@ -52,15 +52,18 @@ app.on('window-all-closed', ()=>{
 
 //macOSでアプリがアクティブになった時にウインドウを作成
 app.on('activate',()=>{
-  if(BrowserWindow.getAllWindows().length!==0){
+  if(BrowserWindow.getAllWindows().length === 0){
     createWindow();
   }
 });
 
-//セキリュティ：新しいウインドウの作成を制限
-app.on('web-content-created',(event,contents)=>{
-  contents.on('new-window', (event,navigationUrl)=>{
-    event.preventDefault();
+//セキュリティ：新しいウインドウの作成を制限
+app.on('web-contents-created', (event, contents) => {
+  // window.open を全て禁止
+  contents.setWindowOpenHandler(() => ({ action: 'deny' }));
+  // 想定外のナビゲーションもブロック
+  contents.on('will-navigate', (e) => {
+    e.preventDefault();
   });
 });
 
